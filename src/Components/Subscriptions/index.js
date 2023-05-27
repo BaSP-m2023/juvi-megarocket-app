4eb1431fb86cfc7dd8f04c8b83179095f5954cb0
leo-balonchard <leobalonchard@gmail.com>
2023-05-27 17:54:11 -0300

    MR-62: modification of index subscriptions

--- src/Components/Subscriptions/index.js
+++ src/Components/Subscriptions/index.js
@@ -1,9 +1,24 @@
 import styles from './subscriptions.module.css';
+import Table from './Table';
+import { useState, useEffect } from 'react';
 
 function Subscriptions() {
+  const [subscriptions, setSubscriptions] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/subscriptions/`)
+      .then((response) => response.json())
+      .then((data) => {
+        setSubscriptions(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
+
   return (
     <section className={styles.container}>
-      <h2>Subscriptions</h2>
+      <Table data={subscriptions.data ? subscriptions.data : []} />
     </section>
   );
 }
