b4d4842dae4b45190d8252a138efbf253be4f82b
Federico Cavallo <fcmde1995@gmail.com>
2023-05-27 13:46:52 -0300

    MR-56:added table component, added edit and delete

---
+++ src/Components/Classes/Table.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+const Table = (props) => {
+  const [selectedClass, setSelectedClass] = useState(null);
+  const handleEdit = (clase) => {
+    props.handleEdit(clase);
+  };
+
+  const handleCancel = () => {
+    setSelectedClass(null);
+  };
+
+  const handleDelete = (id) => {
+    const confirmDelete = window.confirm('Are you sure you want to delete this item?');
+    if (confirmDelete) {
+      props.handleDelete(id);
+    }
+  };
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Activity</th>
+          <th>Trainer</th>
+          <th>Day</th>
+          <th>Hour</th>
+          <th>Slot</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.classesData.map((clase) => (
+          <tr key={clase._id}>
+            <td>{clase.activity.name}</td>
+            <td>{clase.trainer.firstName}</td>
+            <td>{clase.day}</td>
+            <td>{clase.hour}</td>
+            <td>{clase.slots}</td>
+            <td>
+              {selectedClass && selectedClass._id === clase._id ? (
+                <>
+                  <button onClick={() => props.handleUpdate(selectedClass)}>Update</button>
+                  <button onClick={handleCancel}>Cancel</button>
+                </>
+              ) : (
+                <>
+                  <button onClick={() => handleEdit(clase)}>Edit</button>
+                  <button onClick={() => handleDelete(clase._id)}>Delete</button>
+                </>
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
--- src/Components/Classes/index.js
+++ src/Components/Classes/index.js
@@ -1,9 +1,78 @@
 import styles from './classes.module.css';
+import Table from './Table';
+import ClassForm from './ClassForm';
+import { useEffect, useState } from 'react';
 
 function Projects() {
+  const [classesData, setClassesData] = useState([]);
+  const [editedClass, setEditedClass] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/class`);
+      const jsonData = await response.json();
+      const classData = jsonData.data;
+      setClassesData(classData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleUpdate = (updatedClass, classId) => {
+    console.log(updatedClass);
+    fetch(`${process.env.REACT_APP_API_URL}/class/${classId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedClass)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        getData();
+        handleCancel();
+        setEditedClass(null);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`${process.env.REACT_APP_API_URL}/class/${id}`, {
+        method: 'DELETE'
+      });
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleEdit = (clase) => {
+    setIsEditing(true);
+    setEditedClass(clase);
+  };
+  const handleCancel = () => {
+    setEditedClass(null);
+    setIsEditing(false);
+  };
+
   return (
     <section className={styles.container}>
       <h2>Classes</h2>
+      {isEditing && editedClass ? (
+        <ClassForm
+          classData={editedClass}
+          handleUpdate={handleUpdate}
+          handleCancel={() => setEditedClass(null)}
+        />
+      ) : (
+        <Table classesData={classesData} handleDelete={handleDelete} handleEdit={handleEdit} />
+      )}
     </section>
   );
 }
