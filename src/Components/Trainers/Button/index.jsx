756b259f16ac6b408303029767bf32238ffb8bcf
Nicolas Kler <nicolasjosekler@gmail.com>
2023-06-02 16:37:22 -0300

    MR-77: Add font color and border color props in button

--- src/Components/Trainers/Button/index.jsx
+++ src/Components/Trainers/Button/index.jsx
@@ -1,22 +1,34 @@
 import PropTypes from 'prop-types';
 import styles from './button.module.css';
 
-const Button = ({ color, text, onClick }) => {
+const Button = (props) => {
   return (
     <div className={styles.btnContainer}>
-      <button onClick={onClick} style={{ backgroundColor: color }} className={styles.btn}>
-        {text}
+      <button
+        onClick={props.onClick}
+        style={{
+          backgroundColor: props.backColor,
+          color: props.color,
+          borderColor: props.bColor
+        }}
+        className={styles.btn}
+      >
+        {props.text}
       </button>
     </div>
   );
 };
 
 Button.defaultProps = {
-  color: 'white'
+  backColor: 'black',
+  color: 'white',
+  bColor: 'white'
 };
 
 Button.propTypes = {
   text: PropTypes.string,
+  backColor: PropTypes.string,
+  bColor: PropTypes.string,
   color: PropTypes.string,
   onClick: PropTypes.func
 };
