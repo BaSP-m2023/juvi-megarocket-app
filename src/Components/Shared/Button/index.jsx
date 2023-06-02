775c6b2b9ae14163f90768e5a227a39a8b3b1751
Nicolas Kler <nicolasjosekler@gmail.com>
2023-06-02 17:33:44 -0300

    MR-77: Add font color and border color props in button

--- src/Components/Shared/Button/index.jsx
+++ src/Components/Shared/Button/index.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
-// eslint-disable-next-line no-unused-vars
+import PropTypes from 'prop-types';
 import styles from './button.module.css';
-const Button = () => {
-  return <Button></Button>;
+
+const Button = (props) => {
+  return (
+    <div className={styles.btnContainer}>
+      <button
+        onClick={props.onClick}
+        style={{
+          backgroundColor: props.backColor,
+          color: props.color,
+          borderColor: props.bColor
+        }}
+        className={styles.btn}
+      >
+        {props.text}
+      </button>
+    </div>
+  );
+};
+
+Button.defaultProps = {
+  backColor: 'black',
+  color: 'white',
+  bColor: 'white'
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  backColor: PropTypes.string,
+  bColor: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func
 };
 
 export default Button;
