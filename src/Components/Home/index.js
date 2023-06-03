4387c42d45756a55e6ab8f95ddf1e5881d0fc8f1
Federico Cavallo <fcmde1995@gmail.com>
2023-06-03 15:11:19 -0300

    MR-79 fixed some files for the router

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,26 +1,9 @@
 import styles from './home.module.css';
-import React, { useState } from 'react';
-import { ModalAlert } from '../Shared';
 
 function Home() {
-  const [mostrarModal, setMostrarModal] = useState(false);
-
-  const handleConfirmar = () => {
-    // Lógica a ejecutar cuando se confirme el modal
-    console.log('Modal confirmado');
-    setMostrarModal(false);
-  };
   return (
     <section className={styles.container}>
       <h2>Home</h2>
-      <div>
-        <button onClick={() => setMostrarModal(true)}>Mostrar Modal</button>
-
-        {mostrarModal && (
-          <ModalAlert texto="Este es el contenido del modal" onConfirmar={handleConfirmar} />
-        )}
-      </div>
-      );
     </section>
   );
 }
