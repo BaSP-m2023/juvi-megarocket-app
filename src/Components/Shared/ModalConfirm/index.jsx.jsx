3f5134cf64c2a4c5d0bf2192498afadb0b297bc5
octavi01 <octaferreyraleon@gmail.com>
2023-06-03 10:55:00 -0300

    MR-83: added index.jsx

--- src/Components/Shared/ModalConfirm/index.jsx.jsx
+++ src/Components/Shared/ModalConfirm/index.jsx.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 // eslint-disable-next-line no-unused-vars
 import styles from './modal-confirm.module.css';
-const ModalConfirm = () => {
-  return <div></div>;
+
+const ModalConfirm = ({ title, message, onConfirm, onCancel }) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.modalContent}>
+        <h2>{title}</h2>
+        <p>{message}</p>
+        <div className={styles.buttonContainer}>
+          <button onClick={onConfirm}>Confirmar</button>
+          <button onClick={onCancel}>Cancelar</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ModalConfirm;
