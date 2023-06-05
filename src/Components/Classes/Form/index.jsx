82ee509f52929f189728659de6673bc2d13d2d1e
leo-balonchard <leobalonchard@gmail.com>
2023-06-05 12:58:08 -0300

    MR-72: change name and logic

---
+++ src/Components/Classes/Form/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import styles from './classesForm.module.css';
+import { useHistory, useParams } from 'react-router-dom';
+
+const FormClasses = () => {
+  const { id } = useParams();
+
+  return ()
+
+}
+
+export default FormClasses;
--- src/Components/Classes/index.jsx
+++ src/Components/Classes/index.jsx
@@ -1,49 +1,11 @@
-import styles from './newclasses.module.css';
-import Table from './Table';
-import { useState, useEffect } from 'react';
+import styles from './Table/classesTable.module.css';
 import { Link } from 'react-router-dom';
 
-const Classes = () => {
-  const [classesData, setClassData] = useState([]);
-
-  useEffect(() => {
-    getDataClasses();
-  }, []);
-
-  const getDataClasses = async () => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/class`);
-      const jsonData = await response.json();
-      const classData = jsonData.data;
-      setClassData(classData);
-    } catch (error) {
-      alert('Error getting Admins.');
-    }
-  };
-
-  const deleteClass = async (id) => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/class/${id}`, {
-        method: 'DELETE'
-      });
-      if (response.ok) {
-        setClassData(classesData.filter((cls) => cls.id !== id));
-        alert('Class deleted correctly!');
-      } else {
-        throw new Error('Error deleting Class.');
-      }
-    } catch (error) {
-      alert('Error deleting Class: ' + error);
-    }
-  };
+const TableClasses = () => {
 
   return (
-    <section className={styles.containerClass}>
-      <h2>Classes</h2>
-      <Link to="./Form">Add Class</Link>
-      <Table data={classesData} deleteClass={deleteClass} />
-    </section>
+
   );
 };
 
-export default Classes;
+export default TableClasses;
