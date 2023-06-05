4e676966e3180fd8179dd8dcdf5ec47cf0b2438f
moradpablo <moradpabloivan@gmail.com>
2023-06-05 12:13:00 -0300

    MR-71:Delete admin error fixed

--- src/Components/Admins/Table/index.jsx
+++ src/Components/Admins/Table/index.jsx
@@ -1,71 +1,47 @@
-import React, { useState } from 'react';
 import styles from './table.module.css';
 import { Link } from 'react-router-dom';
 import Button from '../../Shared/Button';
-import ModalConfirm from '../../Shared/ModalConfirm';
 
 const Table = (props) => {
-  const [modalMessage, setModalMessage] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   const confirm = (id) => {
     props.deleteAdmin(id);
   };
-  const cancel = () => {
-    setIsModalOpen(false);
-  };
 
   return (
-    <>
-      <table className={styles.adminsTable}>
-        <thead>
-          <tr>
-            <th className={styles.thAdmins}>Name</th>
-            <th className={styles.thAdmins}>Email</th>
-            <th className={styles.thAdmins}>DNI</th>
-            <th className={styles.thAdmins}>Phone</th>
-            <th className={styles.thAdmins}>City</th>
-            <th className={styles.thAdmins}>Actions</th>
+    <table className={styles.adminsTable}>
+      <thead>
+        <tr>
+          <th className={styles.thAdmins}>Name</th>
+          <th className={styles.thAdmins}>Email</th>
+          <th className={styles.thAdmins}>DNI</th>
+          <th className={styles.thAdmins}>Phone</th>
+          <th className={styles.thAdmins}>City</th>
+          <th className={styles.thAdmins}>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((admin) => (
+          <tr key={admin._id}>
+            <td className={styles.tdAdmins}>{admin.firstName + ' ' + admin.lastName}</td>
+            <td className={styles.tdAdmins}>{admin.email}</td>
+            <td className={styles.tdAdmins}>{admin.dni}</td>
+            <td className={styles.tdAdmins}>{admin.phone}</td>
+            <td className={styles.tdAdmins}>{admin.city}</td>
+            <td className={styles.tdAdmins}>
+              <Button
+                type="delete"
+                onClick={() => {
+                  confirm(admin._id);
+                }}
+              ></Button>
+              <Link to={`/admins/AdminForm/${admin._id}`}>
+                <Button type="edit">Edit</Button>
+              </Link>
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {props.data.map((admin) => (
-            <tr key={admin._id}>
-              <td className={styles.tdAdmins}>{admin.firstName + ' ' + admin.lastName}</td>
-              <td className={styles.tdAdmins}>{admin.email}</td>
-              <td className={styles.tdAdmins}>{admin.dni}</td>
-              <td className={styles.tdAdmins}>{admin.phone}</td>
-              <td className={styles.tdAdmins}>{admin.city}</td>
-              <td className={styles.tdAdmins}>
-                <Button
-                  type="delete"
-                  onClick={() => {
-                    setIsModalOpen(true);
-                    setModalMessage({
-                      message: 'Are you sure you want to delete this item?',
-                      title: 'Delete Admin'
-                    });
-                  }}
-                >
-                  Delete
-                </Button>
-                <Link to={`/admins/AdminForm/${admin._id}`}>
-                  <Button type="edit">Edit</Button>
-                </Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {isModalOpen && modalMessage && (
-        <ModalConfirm
-          onConfirm={confirm}
-          onCancel={cancel}
-          message={modalMessage.message}
-          title={modalMessage.title}
-        />
-      )}
-    </>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
--- src/Components/Admins/index.js
+++ src/Components/Admins/index.js
@@ -3,12 +3,13 @@ import Table from './Table/index.jsx';
 import styles from './admins.module.css';
 import { Link } from 'react-router-dom';
 import Button from '../Shared/Button/index.jsx';
-import ModalAlert from '../Shared/ModalAlert/index.jsx';
-
+import { ModalAlert, ModalConfirm } from '../Shared/index.jsx';
 function Admins() {
   const [adminsData, setAdminsData] = useState([]);
   const [modalText, setModalText] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalDeleteOpen, setIsModalDeleteOpen] = useState(false);
+  const [idToDelete, setidToDelete] = useState('');
 
   const getAdmins = async () => {
     try {
@@ -25,36 +26,49 @@ function Admins() {
     getAdmins();
   }, []);
 
-  const deleteAdmin = async (id) => {
+  const deleteAdmin = async () => {
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/admins/${id}`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/admins/${idToDelete}`, {
         method: 'DELETE'
       });
       if (response.ok) {
-        setAdminsData(adminsData.filter((admin) => admin.id !== id));
+        setAdminsData(adminsData.filter((admin) => admin._id !== idToDelete));
         setModalText('Admin deleted correctly!');
         setIsModalOpen(true);
       } else {
         throw new Error('Error deleting Admin.');
       }
     } catch (error) {
-      setModalText(`Error deleting Admin: ${error}`);
+      setModalText(`Error : ${error}`);
       setIsModalOpen(true);
     }
   };
   const closeModal = () => {
     setIsModalOpen(false);
+    setIsModalDeleteOpen(false);
+  };
+  const onDelete = (id) => {
+    setIsModalDeleteOpen(true);
+    setidToDelete(id);
   };
 
   return (
     <section className={styles.container}>
       <h2>Admins</h2>
       <Link to="/admins/AdminForm">
-        <Button type="add" resource="admin" styles>
+        <Button type="add" resource="admin">
           Add Admin
         </Button>
       </Link>
-      <Table data={adminsData} deleteAdmin={deleteAdmin} />
+      <Table data={adminsData} deleteAdmin={onDelete} />
+      {isModalDeleteOpen && (
+        <ModalConfirm
+          onConfirm={deleteAdmin}
+          onCancel={closeModal}
+          message="Are you sure you want to delete this admin?"
+          title="Delete Admin"
+        />
+      )}
       {isModalOpen && <ModalAlert text={modalText} onClick={closeModal} />}
     </section>
   );
