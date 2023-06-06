21c3ba66838aad766d66ffa3d1ab9a632bdd896b
leo-balonchard <leobalonchard@gmail.com>
2023-06-06 18:19:47 -0300

    MR-98: fix table

--- src/Components/Shared/Table/index.jsx
+++ src/Components/Shared/Table/index.jsx
@@ -1,18 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './table.module.css';
-import Button from '../Button';
+import { Button, ModalConfirm } from '../../Shared';
 import { Link } from 'react-router-dom';
 
 const SharedTable = ({ data, handleDelete, editLink }) => {
-  const handleDeleteTable = (id) => {
-    const confirmDelete = window.confirm('Are you sure you want to delete this item?');
-    if (confirmDelete) {
-      handleDelete(id);
-    }
-  };
   if (!data || data.length === 0) {
     return <div>No data available</div>;
   }
+
   const excludedProperties = ['_id', 'isActive', '__v'];
   const propertyNames = Object.keys(data[0]).filter(
     (propertyName) => !excludedProperties.includes(propertyName)
@@ -39,31 +34,69 @@ const SharedTable = ({ data, handleDelete, editLink }) => {
   };
 
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          {propertyNames.map((propertyName) => (
-            <th key={propertyName}>{propertyName}</th>
-          ))}
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
+    <div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
             {propertyNames.map((propertyName) => (
-              <td key={propertyName}>{renderCellValue(item[propertyName])}</td>
+              <th key={propertyName}>{propertyName}</th>
             ))}
-            <td>
-              <Link to={editLink + item._id}>
-                <Button type="edit" />
-              </Link>
-              <Button type="delete" onClick={() => handleDeleteTable(item._id)} />
-            </td>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <TableRow
+              key={index}
+              item={item}
+              propertyNames={propertyNames}
+              editLink={editLink}
+              handleDelete={handleDelete}
+              renderCellValue={renderCellValue}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const TableRow = ({ item, propertyNames, editLink, handleDelete, renderCellValue }) => {
+  const [showAlert, setshowAlert] = useState(false);
+
+  const confirmDeleteHandler = (id) => {
+    handleDelete(id);
+    showAlertHandler();
+  };
+
+  const cancelDeleteHandler = () => {
+    showAlertHandler();
+  };
+
+  const showAlertHandler = () => {
+    setshowAlert(!showAlert);
+  };
+
+  return (
+    <tr>
+      {propertyNames.map((propertyName) => (
+        <td key={propertyName}>{renderCellValue(item[propertyName])}</td>
+      ))}
+      <td>
+        <Link to={editLink + item._id}>
+          <Button type="edit" />
+        </Link>
+        <Button type="delete" onClick={showAlertHandler} />
+        {showAlert && (
+          <ModalConfirm
+            title="Confirm"
+            message="Are you sure you want to delete this item?"
+            onConfirm={() => confirmDeleteHandler(item._id)}
+            onCancel={cancelDeleteHandler}
+          />
+        )}
+      </td>
+    </tr>
   );
 };
 
