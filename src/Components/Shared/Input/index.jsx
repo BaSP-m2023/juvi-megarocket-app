312afd87d88208db798bae7378bb668eddd334d7
Gianluca Agrano <gianlucka1@gmail.com>
2023-06-05 21:38:50 -0300

    MR-75: Added label and div container, also added input types

--- src/Components/Shared/Input/index.jsx
+++ src/Components/Shared/Input/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import styles from './input.module.css';
-const Input = ({ name, onChange }) => {
-  return <input name={name} type="text" className={styles.input} onChange={onChange}></input>;
+const Input = ({ labelText, type, name, value, onChange }) => {
+  return (
+    <div className={styles.container}>
+      <label className={styles.label}>{labelText}</label>
+      <input
+        className={styles.input}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+      ></input>
+    </div>
+  );
 };
 
 export default Input;
