41ea45c7b61128e87b567578662245071fb58873
Gianluca Agrano <gianlucka1@gmail.com>
2023-06-13 09:38:39 -0300

    MR-116: Added methods to thunks.js

---
+++ src/redux/subscriptions/thunks.js
@@ -0,0 +1,112 @@
+import {
+  getSubscriptionsError,
+  getSubscriptionsPending,
+  getSubscriptionsSuccess,
+  deleteSubscriptionsError,
+  deleteSubscriptionsSuccess,
+  deleteSubscriptionsPending,
+  postSubscriptionsError,
+  postSubscriptionsPending,
+  postSubscriptionsSuccess,
+  putSubscriptionsError,
+  putSubscriptionsSuccess,
+  putSubscriptionsPending,
+  getByIdSubscriptionError,
+  getByIdSubscriptionSuccess,
+  getByIdSubscriptionPending
+} from './actions';
+
+export const getSubscriptions = () => {
+  return async (dispatch) => {
+    try {
+      dispatch(getSubscriptionsPending());
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/subscriptions`);
+      const responseJson = await response.json();
+      if (responseJson.error) {
+        throw new Error(responseJson.message);
+      }
+      dispatch(getSubscriptionsSuccess(responseJson.data));
+    } catch (error) {
+      dispatch(getSubscriptionsError(error));
+    }
+  };
+};
+
+export const getSubscriptionsById = (id) => {
+  return async (dispatch) => {
+    try {
+      dispatch(getByIdSubscriptionPending());
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/subscriptions${id}`);
+      const responseJson = await response.json();
+      if (responseJson.error) {
+        throw new Error(responseJson.message);
+      }
+      dispatch(getByIdSubscriptionSuccess(responseJson.data));
+    } catch (error) {
+      dispatch(getByIdSubscriptionError(error));
+    }
+  };
+};
+
+export const deleteSubscription = (id) => {
+  return async (dispatch) => {
+    try {
+      dispatch(deleteSubscriptionsPending());
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/class/${id}`, {
+        method: 'DELETE'
+      });
+      const responseJson = await response.json();
+      if (responseJson.error) {
+        throw new Error(responseJson.message);
+      }
+      const { data, message } = responseJson;
+      dispatch(deleteSubscriptionsSuccess({ data, message }));
+    } catch (error) {
+      dispatch(deleteSubscriptionsError(error.message));
+    }
+  };
+};
+
+export const addSubscription = (formData) => {
+  return async (dispatch) => {
+    try {
+      dispatch(postSubscriptionsPending());
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/subscriptions`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const responseJson = await response.json();
+      if (responseJson.error) {
+        throw new Error(responseJson.error);
+      }
+      dispatch(postSubscriptionsSuccess(responseJson.data));
+    } catch (error) {
+      dispatch(postSubscriptionsError(error));
+    }
+  };
+};
+
+export const editSubscription = (id, formData) => {
+  return async (dispatch) => {
+    try {
+      dispatch(putSubscriptionsPending());
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/subscriptions/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const responseJson = await response.json();
+      if (responseJson.error) {
+        throw new Error(responseJson.message);
+      }
+      dispatch(putSubscriptionsSuccess(responseJson.data));
+    } catch (error) {
+      dispatch(putSubscriptionsError(error));
+    }
+  };
+};
