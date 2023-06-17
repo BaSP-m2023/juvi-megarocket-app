608683a471c2b7607d1eadc9261ce9b4dc1bf828
Nicolas Simonetti <racingclub_nico@hotmail.com>
2023-06-16 21:17:52 -0300

    MR-132: Shared input modified with new name field, member form edited with an example

--- src/Components/Shared/Input/index.jsx
+++ src/Components/Shared/Input/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import styles from './input.module.css';
+import { useForm } from 'react-hook-form';
+
 const Input = ({ labelText, type, name, value, onChange, placeholder }) => {
+  const { register } = useForm();
+
   return (
     <div className={styles.container}>
       <label className={styles.label}>{labelText}</label>
       <input
         className={styles.input}
-        name={name}
+        {...register(name)}
         type={type}
         value={value}
         onChange={onChange}
