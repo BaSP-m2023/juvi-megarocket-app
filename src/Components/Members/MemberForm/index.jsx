694bdc6ff43f5ba0032c1f0a7a248e03bd945979
Nicolas Simonetti <racingclub_nico@hotmail.com>
2023-06-18 16:45:50 -0300

    MR-137: member react-hook-form implemented, also change some shared input functionalities

--- src/Components/Members/MemberForm/index.jsx
+++ src/Components/Members/MemberForm/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getMemberById, putMember, addMember } from 'Components/redux/members/thunks';
+import { getMemberById, putMember, addMember } from 'redux/members/thunks';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import styles from 'Components/Members/MemberForm/form.module.css';
@@ -17,33 +17,58 @@ const MemberForm = (props) => {
   const [showPassword, setShowPassword] = useState(false);
   const data = useSelector((state) => state.members);
   const dispatch = useDispatch();
-  const [memb, setMemb] = useState({});
   const [text, setText] = useState('');
 
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors }
   } = useForm({
-    resolver: joiResolver(schema)
+    resolver: joiResolver(schema),
+    mode: 'onChange',
+    defaultValues: {
+      firstName: data.item?.firstName ?? '',
+      lastName: data.item?.lastName ?? '',
+      dni: data.item?.dni ?? '',
+      phone: data.item?.phone ?? '',
+      email: data.item?.email ?? '',
+      city: data.item?.city ?? '',
+      birthDate: data.item?.birthDate ?? '',
+      postalCode: data.item?.postalCode ?? '',
+      password: data.item?.password ?? '',
+      memberships: data.item?.memberships ?? 'Only Classes'
+    }
   });
 
   const { id } = useParams();
 
   useEffect(() => {
-    console.log(data.item);
-    console.log(memb);
     if (id) {
-      console.log('Si, ID!!!');
       dispatch(getMemberById(id));
-      setMemb(data.item);
       setText('Edit member');
     } else {
-      console.log('Sad, NO ID!!!');
-      setMemb({});
+      data.item = {};
       setText('Add member');
     }
-  }, [data.error, msg, data.item.firstName, memb]);
+  }, []);
+
+  useEffect(() => {
+    if (data.item) {
+      reset({
+        firstName: data.item?.firstName ?? '',
+        lastName: data.item?.lastName ?? '',
+        dni: data.item?.dni ?? '',
+        phone: data.item?.phone ?? '',
+        email: data.item?.email ?? '',
+        city: data.item?.city ?? '',
+        birthDate: data.item?.birthDate ?? '',
+        postalCode: data.item?.postalCode ?? '',
+        password: data.item?.password ?? '',
+        memberships: data.item?.memberships ?? 'Only Classes'
+      });
+    }
+  }, [data.item]);
 
   const togglePassword = () => {
     setShowPassword(!showPassword);
@@ -59,16 +84,9 @@ const MemberForm = (props) => {
     }
   };
 
-  const handleChange = (event) => {
-    setMemb({
-      ...data.item,
-      [event.target.name]: event.current.value
-    });
-  };
-
   const onSubmit = async (data) => {
+    console.log(data);
     try {
-      console.log(await data);
       if (text === 'Add member') {
         dispatch(addMember(data, switchModal));
       } else {
@@ -79,147 +97,135 @@ const MemberForm = (props) => {
     }
   };
 
-  const onInvalid = (errors) => console.error(errors);
+  const onInvalid = (errors) => console.log(errors);
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit(onSubmit, onInvalid)}>
-      <div className={styles.formContainer}>
-        <h1>{text}</h1>
-        <fieldset className={styles.fieldset}>
-          <Input
-            labelText="First Name"
-            className={styles.input}
-            name={'firstName'}
-            type="text"
-            value={memb?.firstName}
-            defaultValue={memb?.firstName ?? ''}
-            onChange={handleChange}
-            placeholder="Ex: Tristan"
-            error={errors.firstName?.message}
-            register={register}
-          />
-        </fieldset>
-        <fieldset className={styles.fieldset}>
-          <Input
-            labelText="Last Name"
-            className={styles.input}
-            name={'lastName'}
-            type="text"
-            defaultValue={memb?.lastName ?? ''}
-            placeholder="Ex: Galvez"
-            error={errors.lastName?.message}
-            register={register}
-          />
-        </fieldset>
-        <fieldset className={styles.fieldset}>
-          <Input
-            labelText="DNI"
-            className={styles.input}
-            name={'dni'}
-            type="number"
-            defaultValue={memb?.dni ?? ''}
-            placeholder="Ex: 33555888"
-            error={errors.dni?.message}
-            register={register}
-          />
-        </fieldset>
-        <fieldset className={styles.fieldset}>
-          <Input
-            labelText="Phone"
-            className={styles.input}
-            name={'phone'}
-            type="number"
-            defaultValue={memb?.phone ?? ''}
-            placeholder="Ex: 11426426"
-            error={errors.phone?.message}
-            register={register}
-          />
-        </fieldset>
-        <fieldset className={styles.fieldset}>
-          <Input
-            labelText="Email"
-            className={styles.input}
-            name={'email'}
-            type="text"
-            defaultValue={memb?.email ?? ''}
-            placeholder="example@example.com"
-            error={errors.email?.message}
-            register={register}
-          />
-        </fieldset>
-        <fieldset className={styles.fieldset}>
-          <Input
-            labelText="City"
-            className={styles.input}
-            name={'city'}
-            type="text"
-            defaultValue={memb?.city ?? ''}
-            placeholder="Ex: Casilda"
-            error={errors.city?.message}
-            register={register}
-          />
-        </fieldset>
-        <fieldset className={styles.fieldset}>
-          <Input
-            labelText="Birth Day"
-            className={styles.input}
-            name={'birthDate'}
-            type="datetime-local"
-            defaultValue={memb?.birthDate ?? '1990-01-01 01-01'}
-            error={errors.birthDate?.message}
-            register={register}
-          />
-        </fieldset>
-        <fieldset className={styles.fieldset}>
-          <Input
-            labelText="Zip"
-            className={styles.input}
-            name={'postalCode'}
-            type="number"
-            defaultValue={memb?.postalCode ?? ''}
-            placeholder="Ex: 2170"
-            error={errors.postalCode?.message}
-            register={register}
-          />
-        </fieldset>
-        <fieldset className={styles.fieldset}>
-          <div className={styles.password}>
+    <div>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div className={styles.formContainer}>
+          <h1>{text}</h1>
+          <fieldset className={styles.fieldset}>
             <Input
-              labelText="Password"
+              labelText="First Name"
               className={styles.input}
-              name={'password'}
-              type={showPassword ? 'text' : 'password'}
-              defaultValue={memb?.password ?? ''}
-              placeholder="Password"
-              error={errors.password?.message}
+              name={'firstName'}
+              type="text"
+              placeholder="Ex: Tristan"
+              error={errors.firstName?.message}
               register={register}
             />
-            <FontAwesomeIcon
-              icon={showPassword ? faEyeSlash : faEye}
-              className={styles.showPasswordIcon}
-              onClick={togglePassword}
+          </fieldset>
+          <fieldset className={styles.fieldset}>
+            <Input
+              labelText="Last Name"
+              className={styles.input}
+              name={'lastName'}
+              type="text"
+              placeholder="Ex: Galvez"
+              error={errors.lastName?.message}
+              register={register}
             />
-          </div>
-        </fieldset>
-        <fieldset className={styles.fieldset}>
-          <label>Membership</label>
-          <select
-            name="memberships"
-            defaultValue={memb?.memberships ?? 'Only Classes'}
-            {...register('memberships')}
-          >
-            <option value="Only Classes">Only Classes</option>
-            <option value="Classic">Classic</option>
-            <option value="Black">Black</option>
-          </select>
-          {errors.memberships && <p>{errors.memberships.message}</p>}
-        </fieldset>
-      </div>
-      <Button type={'submit'} resource={'Member'} />
-      <Button type={'cancel'} onClick={() => props.history.push('/members')} />
-      {modal && <ModalAlert text={msg} onClick={() => setModal(!modal)} />}
-      {modalDone && <ModalAlert text={msg} onClick={() => props.history.push('/members')} />}
-    </form>
+          </fieldset>
+          <fieldset className={styles.fieldset}>
+            <Input
+              labelText="DNI"
+              className={styles.input}
+              name={'dni'}
+              type="number"
+              placeholder="Ex: 33555888"
+              error={errors.dni?.message}
+              register={register}
+            />
+          </fieldset>
+          <fieldset className={styles.fieldset}>
+            <Input
+              labelText="Phone"
+              className={styles.input}
+              name={'phone'}
+              type="number"
+              placeholder="Ex: 11426426"
+              error={errors.phone?.message}
+              register={register}
+            />
+          </fieldset>
+          <fieldset className={styles.fieldset}>
+            <Input
+              labelText="Email"
+              className={styles.input}
+              name={'email'}
+              type="text"
+              placeholder="example@example.com"
+              error={errors.email?.message}
+              register={register}
+            />
+          </fieldset>
+          <fieldset className={styles.fieldset}>
+            <Input
+              labelText="City"
+              className={styles.input}
+              name={'city'}
+              type="text"
+              placeholder="Ex: Casilda"
+              error={errors.city?.message}
+              register={register}
+            />
+          </fieldset>
+          <fieldset className={styles.fieldset}>
+            <Input
+              labelText="Birth Day"
+              className={styles.input}
+              name={'birthDate'}
+              type="datetime-local"
+              error={errors.birthDate?.message}
+              register={register}
+            />
+          </fieldset>
+          <fieldset className={styles.fieldset}>
+            <Input
+              labelText="Zip"
+              className={styles.input}
+              name={'postalCode'}
+              type="number"
+              placeholder="Ex: 2170"
+              error={errors.postalCode?.message}
+              register={register}
+            />
+          </fieldset>
+          <fieldset className={styles.fieldset}>
+            <div className={styles.password}>
+              <Input
+                labelText="Password"
+                className={styles.input}
+                name={'password'}
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Password"
+                error={errors.password?.message}
+                register={register}
+              />
+              <FontAwesomeIcon
+                icon={showPassword ? faEyeSlash : faEye}
+                className={styles.showPasswordIcon}
+                onClick={togglePassword}
+              />
+            </div>
+          </fieldset>
+          <fieldset className={styles.fieldset}>
+            <label>Membership</label>
+            <select name="memberships" {...register('memberships')}>
+              <option value="Only Classes">Only Classes</option>
+              <option value="Classic">Classic</option>
+              <option value="Black">Black</option>
+            </select>
+            {errors.memberships && <p>{errors.memberships.message}</p>}
+          </fieldset>
+        </div>
+        <Button type={'submit'} resource={'Member'} />
+        <Button type={'cancel'} onClick={() => props.history.push('/members')} />
+        {modal && <ModalAlert text={msg} onClick={() => setModal(!modal)} />}
+        {modalDone && <ModalAlert text={msg} onClick={() => props.history.push('/members')} />}
+      </form>
+      <Button className={styles.addButton} type="reset" onClick={() => reset()}></Button>
+    </div>
   );
 };
 
--- src/Components/Shared/Input/index.jsx
+++ src/Components/Shared/Input/index.jsx
@@ -1,13 +1,12 @@
 import styles from './input.module.css';
 
-const Input = ({ labelText, type, name, value, error, placeholder, register }) => {
+const Input = ({ labelText, type, name, error, placeholder, register }) => {
   return (
     <div className={styles.container}>
       <label className={styles.label}>{labelText}</label>
       <input
         className={styles.input}
         type={type}
-        value={value}
         name={name}
         placeholder={placeholder}
         {...register(name)}
