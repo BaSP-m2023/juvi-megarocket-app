40f26b53968c197a838db41d4dd46ae5be98dce2
Gianluca Agrano <gianlucka1@gmail.com>
2023-06-20 16:05:29 -0300

    MR-142: Added routes and form added

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -14,6 +14,7 @@ const classForm = lazy(() => import('Components/Admin/Classes/Form'));
 const classList = lazy(() => import('Components/Admin/Classes'));
 const memberForm = lazy(() => import('Components/Admin/Members/MemberForm'));
 const memberList = lazy(() => import('Components/Admin/Members'));
+const memberProfile = lazy(() => import('Components/Member/Profile'));
 const subscriptionForm = lazy(() => import('Components/Admin/Subscriptions/Form'));
 const subscriptionList = lazy(() => import('Components/Admin/Subscriptions'));
 const superAdminForm = lazy(() => import('Components/SuperAdmins/Form/Index'));
@@ -68,6 +69,7 @@ function Layout() {
                 render={() => (
                   <>
                     <Route exact path="/members/activities" component={activityMember} />
+                    <Route exact path="/members/profile" component={memberProfile} />
                   </>
                 )}
               />
