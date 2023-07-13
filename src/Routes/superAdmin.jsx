89cfaf38730de52b7d89ebc226de5d026f003812
octavi01 <octaferreyraleon@gmail.com>
2023-07-13 16:16:00 -0300

    MR-194: added some logic to super-admin

--- src/Routes/superAdmin.jsx
+++ src/Routes/superAdmin.jsx
@@ -24,6 +24,7 @@ const SuperAdminRoutes = () => {
         <Route exact path={`${url}/`} component={Home} />
         <Route exact path={`${url}/admins`} component={Admin} />
         <Route exact path={`${url}/admins/form`} component={AdminsForm} />
+        <Route exact path={`${url}/admins/form/:id`} component={AdminsForm} />
       </Switch>
     </Layout>
   );
